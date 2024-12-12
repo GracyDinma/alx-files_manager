63701881d24d72fd934455f4e4bad53ad65ad175
root <root@DESKTOP-ODC38F9.localdomain>
2024-12-12 21:05:39 +0100

    Lint error

--- utils/redis.js
+++ utils/redis.js
@@ -1,39 +1,40 @@
 import { createClient } from 'redis';
 import { promisify } from 'util';
 
-
 class RedisClient {
   /**
-   * Create a new RedisClient instance 
+   * Create a new RedisClient instance
    */
   constructor() {
     this.client = createClient();
     this.isClientConnect = true;
     this.client.on('error', (err) => {
-      console.error("Redis failed to connect", err.message);
-      this.isClientConnect = false
-    })
+      console.error('Redis failed to connect', err.message);
+      this.isClientConnect = false;
+    });
     this.client.on('connect', () => {
-      this.isClientConnect =  true;
+      this.isClientConnect = true;
     });
   }
+
   isAlive() {
     return this.isClientConnect;
   }
 
   async get(key) {
-    const getKey = promisify(this.client.get).bind(this.client)(key)
+    const getKey = promisify(this.client.get).bind(this.client)(key);
     return getKey;
   }
+
   async set(key, value, duration) {
     const setKey = await promisify(this.client.setex)
       .bind(this.client)(key, duration, value);
-      return setKey
+    return setKey;
   }
 
   async del(key) {
     const delKey = await promisify(this.client.del).bind(this.client)(key);
-    return delKey
+    return delKey;
   }
 }
 
